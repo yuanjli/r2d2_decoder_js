f43537d4fe0e94564febfb6d4a55c55e91c266a5
yuanjli <liyuanjienetwork@gmail.com>
2018-07-25 09:52:09 -0700

    the solution.js file is finished

---
+++ recoder.js
@@ -0,0 +1,30 @@
+// this is file is the solution to the decoder activity: 
+// July 25, 2018 
+
+/*
+2 beeps, 6 boops
+0 beeps, 5 boops
+9 beeps, 3 boops
+4 beeps, 8 boops
+10 beeps, 5 boops
+BOP! (pretty sure this is a space!)
+11 beeps, 12 boops
+5 beeps, 5 boops
+1 beep, 17 boops
+5 beeps, 7 boops
+4 beeps, 0 boops
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log()
---
+++ solution.js
@@ -0,0 +1,15 @@
+// let beeps = 2; 
+// let boops = 6; 
+// let total = beeps + boops;
+
+// console.log(total); 
+
+
+
+var beeps = [2, 0, 9, 4, 10, 11, 5, 1, 5, 4]
+var boops = [6, 5, 3, 8, 5, 12, 5, 17, 7, 0]
+
+var i;
+for ( i=0; i<beeps.length; i++){
+	console.log(beeps[i]+boops[i]);
+}
